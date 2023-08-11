567ae9f528fe662e45c0243e838c4f25da66b099
chrisvoncsefalvay <chris@chrisvoncsefalvay.com>
2023-08-11 17:05:03 -0600

    Small harness tweak to enable FASTA inputs.

--- utils/harness.py
+++ utils/harness.py
@@ -1,7 +1,8 @@
 class RosalindProblem:
     """Harness for running a Rosalind problem."""
-    def __init__(self, func, input_file):
+    def __init__(self, func, input_file, fasta=False):
         self.func = func
+        self.fasta = fasta
         self.input_file = input_file
         self.output_file = input_file.replace("rosalind_", "").replace(".txt", "_solution.txt")
 
@@ -9,12 +10,16 @@ class RosalindProblem:
         """Process the input file and write the output file."""
         with open(self.input_file, 'r') as infile, open(self.output_file, 'w') as outfile:
             arguments = []
-            for line in infile:
-                arguments.append(line.strip())
+
+            if self.fasta:
+                arguments = [infile.read()]
+            else:
+                for line in infile:
+                    arguments.append(line.strip())
 
             print(arguments)
             result = self.func(*arguments)
-            if isinstance(result, list):
+            if isinstance(result, list) or isinstance(result, tuple):
                 result = [str(item) for item in result]
                 for item in result:
                     outfile.write(str(item) + '\n')
